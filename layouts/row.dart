f21a24af4740901aca87129e71e2a77513f06138
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-06-24 17:29:50 +0900

    Create row.dart

---
+++ layouts/row.dart
@@ -0,0 +1,22 @@
+Row(
+  children: [
+    Container(
+      color: Colors.orange,
+      child: FlutterLogo(
+        size: 60.0,
+      ),
+    ),
+    Container(
+      color: Colors.blue,
+      child: FlutterLogo(
+        size: 60.0,
+      ),
+    ),
+    Container(
+      color: Colors.purple,
+      child: FlutterLogo(
+        size: 60.0,
+      ),
+    ),
+  ],
+)
